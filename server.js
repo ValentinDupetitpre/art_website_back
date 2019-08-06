1af94c1d95783fcc8625088454a46133c68dd18c
ValentinDupetitpre <valentin.dupetitpre@gmail.com>
2019-08-06 22:40:16 +0200

    update collection image saving

--- server.js
+++ server.js
@@ -245,9 +245,18 @@ CollectionResource.update.write.before(function (req, res, context) {
     return new Promise(function (resolve, reject) {
         const uri = req.body.pic.split(';base64,').pop()
         const img = Buffer.from(uri, 'base64');
-        req.body.pic = img
-
-        resolve(context.continue)
+        sharp(img)
+        .resize({ 
+            width: 450,
+            height: 450,
+            fit: 'outside'
+        })
+        .toBuffer({ resolveWithObject: true })
+        .then(({ data, info }) => { 
+            req.body.pic = data
+            resolve(context.continue)
+         })
+        .catch(err => console.log(err));
     })
 });
 
@@ -255,9 +264,19 @@ CollectionResource.create.write.before(function (req, res, context) {
     return new Promise(function (resolve, reject) {
         const uri = req.body.pic.split(';base64,').pop()
         const img = Buffer.from(uri, 'base64');
-        req.body.pic = img
+        sharp(img)
+        .resize({ 
+            width: 450,
+            height: 450,
+            fit: 'outside'
+        })
+        .toBuffer({ resolveWithObject: true })
+        .then(({ data, info }) => { 
+            req.body.pic = data
+            resolve(context.continue)
+         })
+        .catch(err => console.log(err));
 
-        resolve(context.continue)
     })
 });
 
