8a9a9ee8153209e920fbc7cdee25ab0d3dff6fb5
ValentinDupetitpre <valentin.dupetitpre@gmail.com>
2019-09-05 09:24:28 +0200

    adding front

---
+++ client/build/precache-manifest.de9a4b62818b7c5d2891698d2be9cc75.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5e6fa7a690cac63a0f4898bdb97e36d",
+    "url": "/static/media/no-image.a5e6fa7a.svg"
+  },
+  {
+    "revision": "66fada1144a0b2f2839d",
+    "url": "/static/js/runtime~main.66fada11.js"
+  },
+  {
+    "revision": "1ad50a72bf10dbb4df05",
+    "url": "/static/js/main.1ad50a72.chunk.js"
+  },
+  {
+    "revision": "f3358b42036758d3c959",
+    "url": "/static/js/3.f3358b42.chunk.js"
+  },
+  {
+    "revision": "945e4949c45971e73ba3",
+    "url": "/static/js/2.945e4949.chunk.js"
+  },
+  {
+    "revision": "0215d10777f8b5fb2acb",
+    "url": "/static/js/1.0215d107.chunk.js"
+  },
+  {
+    "revision": "1ad50a72bf10dbb4df05",
+    "url": "/static/css/main.6c3dc724.chunk.css"
+  },
+  {
+    "revision": "f3358b42036758d3c959",
+    "url": "/static/css/3.24907e89.chunk.css"
+  },
+  {
+    "revision": "945e4949c45971e73ba3",
+    "url": "/static/css/2.04bebedc.chunk.css"
+  },
+  {
+    "revision": "fe25a0cc8344abc66db32a4a6ff51499",
+    "url": "/index.html"
+  }
+];
